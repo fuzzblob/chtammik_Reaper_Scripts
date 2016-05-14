3c629ca95902f8aae37382859e921807f24c22ee
chtammik <christopher@tammik.ca>
2016-05-13 21:52:47 -0700

    added Items Editing/chtammik_Move selected item [direction] [duration] seconds.eel added Cursor/chtammik_Move cursor right to next item edge on same track.eel added Utility/chtammik_Show itemnumber in console.eel moved Tracks to Track Properties Added info to README.md

---
+++ Items Editing/chtammik_Move selected items left 2 seconds.lua
@@ -0,0 +1,41 @@
+-- ReaScript Name: chtammik_Move selected items [direction] [duration] seconds.lua
+-- Description: See title
+-- Instructions: slect region(s), run
+-- Author: chtammik
+-- Author URI: http://tammik.ca
+-- Repository: GitHub > chtammik > chtammik_Reaper_Scripts
+-- Repository URI: https://github.com/chtammik/chtammik_Reaper_Scripts
+-- File URI:
+-- Licence: GPL v3
+-- Forum Thread: Script: Script name
+-- Forum Thread URI: http://forum.cockos.com/***.html
+-- REAPER: 5.18
+-- Extensions: None
+-- Version: 1.0
+
+posn_move = -2.0	-- set item position translation, left is negative (-1.7 was requested)
+msg_flag = true		-- flag to report errors to console
+
+function msg(val)        --define msg function...
+    if msg_flag then reaper.ShowConsoleMsg(tostring(val).."\n") end
+end
+
+function get_posn(sel)			-- get position of sel item (sel number)
+	return reaper.GetMediaItemInfo_Value(reaper.GetSelectedMediaItem(0, sel), "D_POSITION")		-- read item position
+end
+
+function move_item(sel, val)			-- move position of sel item (sel number)
+	reaper.SetMediaItemInfo_Value(reaper.GetSelectedMediaItem(0, sel), "D_POSITION", val)		-- write item position
+end
+
+reaper.Undo_BeginBlock() -- Hold undo block to main code...
+num_sel_items = reaper.CountSelectedMediaItems(0)	-- count selected items
+if num_sel_items >0 then -- if items selected...
+	for i = 0, num_sel_items -1 do	-- cycle through selected items
+		item_posn = get_posn(i)		-- get position of sel item number
+		if item_posn + posn_move >=0 then move_item(i, item_posn + posn_move) else msg("An item was not moved (neg pos'n)") end	-- move if won't create neg position
+	end
+else
+	msg("No selected items...") 	 --if no sel items, msg to console
+end
+reaper.Undo_EndBlock("Move "..tostring(num_sel_items).." sel item(s) "..tostring(posn_move).."s", -1) -- close undo block and give annotation to undo stack.
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # chtammik_Reaper_Scripts
 scripts written to add functionality to the Reaper DAW
+This Repository currently contains the following scripts
+
+Items:
+	chtammik_Move selected items [direction] [X] seconds.eel
+		[left,right]; [0.1; 0.5; 1; 2; 5; 10]
+
+Navigation:
+	chtammik_Move cursor right to next item edge on same track.eel
+		[work in progress]
+
+Tracks:
+	chtammik_XOR exclusive solo toggle selected tracks.eel
+		Pro Tools like exlusive solo toggle
+
+Utility:
+	chtammik_Show itemnumber in console.eel
